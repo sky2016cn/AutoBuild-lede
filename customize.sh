6304e9f7bd1db808e4e3782b8532cd5b6dea7f81
sky2016cn <58769170+sky2016cn@users.noreply.github.com>
2023-06-08 09:02:10 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
+# Copyright (c) 2019-2023 sky2016cn
+# Author: sky2016cn
+# licensed under the MIT License
+# See /LICENSE for more information
+# Description: Build OpenWrt using GitHub Actions
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
