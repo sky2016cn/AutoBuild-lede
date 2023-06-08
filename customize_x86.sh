2269caa25279eb22cf5deadcf82ddd1ef382228b
sky2016cn <58769170+sky2016cn@users.noreply.github.com>
2023-06-09 02:07:21 +0800

    Update customize_x86.sh

--- customize_x86.sh
+++ customize_x86.sh
@@ -13,13 +13,13 @@ sed -i 's/192.168.1.1/192.168.9.1/g' openwrt/package/base-files/files/bin/config
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3.poweroff
-git clone https://github.com/sky2016cn/luci-app-poweroff.git package/luci-app-poweroff
+git clone https://github.com/sky2016cn/luci-app-poweroff.git openwrt/package/luci-app-poweroff
 
 #4.OpenClash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git openwrt/package/OpenClash
 
 #5.OpenAppFilter
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/destan19/OpenAppFilter.git openwrt/package/OpenAppFilter
 
 #6. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
